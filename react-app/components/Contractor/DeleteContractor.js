5147f3eff78bb9660a91d5601ae4f02a97df67a0
fms-byte <fsh69711@gmail.com>
2024-05-10 23:34:29 +0600

    Fixed Delete Contractor

--- react-app/components/Contractor/DeleteContractor.js
+++ react-app/components/Contractor/DeleteContractor.js
@@ -2,24 +2,27 @@ import { Dialog, Transition } from '@headlessui/react'
 import React, { Fragment, useState } from 'react'
 import { getBaseUrl } from '../../utils/url'
 import Button from '../common/Button'
+import axios from 'axios'
 const DeleteContractor = ({ contractorId, ...props }) => {
   const [isOpen, setIsOpen] = useState(false)
   const handleClose = () => setIsOpen(false)
   const handleOpen = () => setIsOpen(true)
 
+
   const handleDelete = async () => {
     try {
       const token = localStorage.getItem('token')
-      await fetch(getBaseUrl() + `/contractor/${contractorId}`, {
-        method: 'DELETE',
+      await axios.delete(getBaseUrl() + `/contractor/${contractorId}`, {
         headers: {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${token}`,
         },
-        body: JSON.stringify({ id: contractorId }),
-      }).then(() => {
+      }).then((res) => {
+        console.log(res)
         handleClose()
         window.location.replace('/contractor')
+      }).catch((error) => {
+        console.log('Error Deleting User:',error)
       })
     } catch (error) {
       console.log(error)
